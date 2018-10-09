86c098fb7aa0be586ef70e907a7d289976b9c3c8
jjjjim <imjjjjim@outlook.com>
2018-10-09 18:51:03 +0800

    working 评论

--- src/components/decision.vue
+++ src/components/decision.vue
@@ -3,17 +3,6 @@
     <section class="content-zone">
       <decisioncontent :detail="detail" :createTime="createTime" :stateImg="stateImg" :avaliable="avaliable" v-if="createTime">
       </decisioncontent>
-      <!-- <div class="timeout" v-if="1 === 3 && !avaliable && !isHost && !participated">
-        <div class="header">
-          <img src="http://ojrbqzf6q.qnssl.com/Fta9bluMYJtPPHxR9dCWibc-s8Jd.svg" alt="timeout" class="timeout-icon">
-          <p class="title">
-            已结束
-          </p>
-        </div>
-        <p class="warning-content">
-          发起的决定会在被发起24小时后自动失效，你不能再参与此决定。
-        </p>
-      </div> -->
       <result :decisionDetail="detail" v-if="showResult">
       </result>
     </section>
@@ -108,6 +97,9 @@
   width: 100%;
   height: 100%;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   .content-zone{
     width: 100%;
     .author{
--- src/components/decision_result.vue
+++ src/components/decision_result.vue
@@ -1,69 +1,103 @@
 <template>
   <div class="decision-result" v-if="showResult">
-    <div class="state">
-      <span class="text against">
-        反对方
-      </span>
-      <div class="score">
-        <span class="count">
-          {{ negativeResult.length }}          
+    <div class="switch-panel">
+      <form @submit="switchPanel" report-submit>
+        <button formType="submit">
+          <img src="http://ojrbqzf6q.qnssl.com/FoEPTqDL8lb7H1JhH_Q4n7rXhD9f.svgz" alt="comment" v-if="currentMode === 'score'">
+          <img src="http://ojrbqzf6q.qnssl.com/FmkQogBo9uo6FvQZ7RZ1r_zkkLig.svgz" alt="score-result" v-else>
+        </button>
+      </form>
+    </div>
+    <block v-if="currentMode === 'score'">
+      <div class="state">
+        <span class="text against">
+          反对方
         </span>
-        <img src="http://ojrbqzf6q.qnssl.com/FqGFl4pu_WB3OQTJz-1dZwisUPcX.svgz" alt="VS" class="icon">
-        <span class="count">
-          {{ positiveResult.length }}
+        <div class="score">
+          <span class="count">
+            {{ negativeResult.length }}          
+          </span>
+          <img src="http://ojrbqzf6q.qnssl.com/FqGFl4pu_WB3OQTJz-1dZwisUPcX.svgz" alt="VS" class="icon">
+          <span class="count">
+            {{ positiveResult.length }}
+          </span>
+        </div>
+        <span class="text support">
+          赞成方
         </span>
       </div>
-      <span class="text support">
-        赞成方
-      </span>
-    </div>
-    <section class="bar positive">
-      <section class="negative" :style="negativeRate">
+      <section class="bar positive">
+        <section class="negative" :style="negativeRate">
+        </section>
       </section>
-    </section>
-    <ul class="members">
-      <li>
-        <ul class="avatars with-name" v-if="negativeResult.length <= 5">
-          <li v-for="(item, index) in negativeResult" :key="index" class="ellipsis" v-if="item">
-            <img class="avatar" :src="item.avatar" alt="avatar">
-            <span>
-              {{ item.name }}
-            </span>
-          </li>
-        </ul>
-        <ul class="avatars no-name" v-else>
-          <li v-for="(item, index) in negativeResult" :key="index" class="ellipsis" v-if="item">
-            <img class="avatar" :src="item.avatar" alt="avatar">
-          </li>
-        </ul>
-      </li>
-      <li>
-        <ul class="avatars with-name" v-if="positiveResult.length <= 5">
-          <li v-for="(item, index) in positiveResult" :key="index" class="ellipsis" v-if="item">
-            <span class="ellipsis">
-              {{ item.name }}
-            </span>
-            <img class="avatar" :src="item.avatar" alt="avatar">
-          </li>
-        </ul>
-        <ul class="avatars no-name" v-else>
-          <li v-for="(item, index) in positiveResult" :key="index" class="ellipsis" v-if="item">
-            <img class="avatar" :src="item.avatar" alt="avatar">
-          </li>
-        </ul>
-      </li>
-    </ul>
+      <ul class="members">
+        <li>
+          <ul class="avatars with-name" v-if="negativeResult.length <= 5">
+            <li v-for="(item, index) in negativeResult" :key="index" class="ellipsis" v-if="item">
+              <img class="avatar" :src="item.avatar" alt="avatar">
+              <span>
+                {{ item.name }}
+              </span>
+            </li>
+          </ul>
+          <ul class="avatars no-name" v-else>
+            <li v-for="(item, index) in negativeResult" :key="index" class="ellipsis" v-if="item">
+              <img class="avatar" :src="item.avatar" alt="avatar">
+            </li>
+          </ul>
+        </li>
+        <li>
+          <ul class="avatars with-name" v-if="positiveResult.length <= 5">
+            <li v-for="(item, index) in positiveResult" :key="index" class="ellipsis" v-if="item">
+              <span class="ellipsis">
+                {{ item.name }}
+              </span>
+              <img class="avatar" :src="item.avatar" alt="avatar">
+            </li>
+          </ul>
+          <ul class="avatars no-name" v-else>
+            <li v-for="(item, index) in positiveResult" :key="index" class="ellipsis" v-if="item">
+              <img class="avatar" :src="item.avatar" alt="avatar">
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </block>
+    <block v-else>
+      <ul>
+        <li v-for="item in comments" :key="item.time">
+          <section class="avatar">
+            <img :src="item.avatar" alt="avatars">
+          </section>
+        </li>
+      </ul>
+    </block>
   </div>
 </template>
 <script>
   export default {
     props: ['decisionDetail'],
+    data () {
+      return {
+        currentMode: 'score'
+      }
+    },
     computed: {
       positiveResult () {
         return (this.decisionDetail.yes) || []
       },
       negativeResult () {
         return (this.decisionDetail.no) || []
+      },
+      comments () {
+        return [
+          {
+            avatar: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3797481993,1929347741&fm=27&gp=0.jpg',
+            name: '小褥子',
+            time: '2018-10-02 09:34:21',
+            content: '嘿嘿嘿。让我们一起摇摆。'
+          }
+        ]
       },
       negativeRate () {
         let rate = 0
@@ -103,6 +137,12 @@
         } else {
           return '双方持平'
         }
+      }
+    },
+    methods: {
+      switchPanel (e) {
+        this.$store.commit('saveFormId', e)
+        this.currentMode = this.currentMode === 'comment' ? 'score' : 'comment'
       }
     }
   }
